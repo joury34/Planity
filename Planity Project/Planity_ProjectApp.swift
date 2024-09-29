621fb4b36b5957bf4ecb7673b20d0cf037f921e3
aalliiaah <aliah0909abdulrhman@gmail.com>
2024-09-29 11:51:28 +0300

    Initial Commit

---
+++ Planity Project/Planity_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Planity_ProjectApp.swift
+//  Planity Project
+//
+//  Created by Aliah Alhameed on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Planity_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
