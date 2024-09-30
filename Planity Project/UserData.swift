c5e7f56443a1aa915e8c2deac067b002e7420bbb
aalliiaah <aliah0909abdulrhman@gmail.com>
2024-09-30 09:32:32 +0300

    userData page

---
+++ Planity Project/UserData.swift
@@ -0,0 +1,18 @@
+//
+//  UserData.swift
+//  Planity Project
+//
+//  Created by Aliah Alhameed on 27/03/1446 AH.
+//
+
+import SwiftUI
+
+struct UserData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserData()
+}
